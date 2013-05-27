a098c5c2d74b87796cca3cccc699ac0fc57525ef
Drew Malin <drewmalin@gmail.com>
2013-05-26 18:25:02 -0700

    add edit

--- Main/__init__.py
+++ Main/__init__.py
@@ -17,7 +17,14 @@ app.add_url_rule('/item/',
                  methods = ['GET', 'POST'])
 app.add_url_rule('/item/<int:item_id>',
                  view_func = ItemView.as_view('item'),
+                 methods = ['GET', 'POST', 'PUT'])
+app.add_url_rule('/item/edit/',
+                 defaults = {'item_id': None},
+                 view_func = ItemMod.as_view('item_mod'),
                  methods = ['GET', 'POST'])
+app.add_url_rule('/item/edit/<int:item_id>',
+                view_func = ItemMod.as_view('item_mod'),
+                methods = ['GET', 'POST'])
 
 from models import *
 Base.metadata.create_all(engine)
--- Main/views.py
+++ Main/views.py
@@ -25,8 +25,26 @@ class ItemView(flask.views.MethodView):
             item = session.query(Item).get(item_id)
             return flask.render_template('item.html', item = item)
         else:
-            return flask.render_template('new_item.html')
+            return flask.redirect(flask.url_for('index'))
+
+class ItemMod(flask.views.MethodView):
+    def get(self, item_id):
+        if item_id != None:
+            item = session.query(Item).get(item_id)
+        else:
+            item = None
+        return flask.render_template('new_item.html', item = item)
+
     def post(self, item_id):
+        if item_id == None:
+            self.myPost()
+            return flask.redirect(flask.url_for('index'))
+        else:
+            self.myPut(item_id)
+            item = session.query(Item).get(item_id)
+            return flask.render_template('item.html', item = item)
+
+    def myPost(self):
         error = 0
         if flask.request.form['name'] == "":
             flask.flash("Name is required!")
@@ -39,11 +57,31 @@ class ItemView(flask.views.MethodView):
             error = 1
         if error > 0:
             return flask.redirect(flask.url_for('item'))
-
+        
         item = Item(flask.request.form['name'],
                     flask.request.form['num'],
                     flask.request.form['quantity'],
                     1, 1)
         session.add(item)
         session.commit()
-        return flask.redirect(flask.url_for('index'))
+        
+    def myPut(self, item_id):
+        error = 0
+        if flask.request.form['name'] == "":
+            flask.flash("Name is required!")
+            error = 1
+        if flask.request.form['num'] == "":
+            flask.flash("Num is required!")
+            error = 1
+        if flask.request.form['quantity'] == "":
+            flask.flash("Quantity is required!")
+            error = 1
+        if error > 0:
+            return flask.redirect(flask.url_for('item'))
+        #session.query(Item).get(item_id).update({'name':flask.request.form['name'], 'num':flask.request.form['num'], 'quantity_on_hand':flask.request.form['quantity']})
+        item = session.query(Item).get(item_id)
+        item.name = flask.request.form['name']
+        item.num = flask.request.form['num']
+        item.quantity_on_hand = flask.request.form['quantity']
+        session.add(item)
+        session.commit()
